ba0e1afa20d4759668047c78634243dddf5c7d39
Wizz <70026238+Wizz1337@users.noreply.github.com>
2022-04-03 13:04:04 +0200

    ~

---
+++ ShareBot.py
@@ -0,0 +1,154 @@
+import ssl, os, requests, time
+from threading import active_count, Thread
+from pystyle import Colorate, Colors, Write
+from random import randint, choice
+from urllib3.exceptions import InsecureRequestWarning
+from http import cookiejar
+from Data.UserAgent import UserAgent
+from Data.Lists import DeviceTypes, Platforms, Channel, ApiDomain
+
+class BlockCookies(cookiejar.CookiePolicy):
+    return_ok = set_ok = domain_return_ok = path_return_ok = lambda self, *args, **kwargs: False
+    netscape = True
+    rfc2965 = hide_cookie2 = False
+
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+ssl._create_default_https_context = ssl._create_unverified_context
+r                                 = requests.Session()
+ThreadCount                       = 0
+TotalSendedShare                   = 0
+TotalFailedReq                    = 0
+DebugMode                         = False
+
+r.cookies.set_policy(BlockCookies())
+
+def Clear():
+    if os.name == 'posix':
+        os.system('clear')
+    elif os.name in ('ce', 'nt', 'dos'):
+        os.system('cls')
+    else:
+        pass
+
+def Title(Content):
+    global DebugMode
+    if os.name in ('posix', 'ce', 'dos'):
+        pass
+    elif os.name == 'nt':
+        os.system(f"title {Content}")
+        return False
+    else:
+        pass
+
+def ReadFile(filename,method):
+    with open(filename,method,encoding='utf8') as f:
+        content = [line.strip('\n') for line in f]
+        return content
+
+def ReadProxiesFile():
+    restartTry = True
+    Path = "./Data/Proxies.txt"
+    while restartTry:
+        try:
+            proxies = ReadFile(Path, 'r')
+            restartTry = False
+            return proxies
+        except:
+            print(Colorate.Horizontal(Colors.red_to_white, f"Failed to open Proxies.txt"))
+            Path = Write.Input("Proxies Path > ", Colors.red_to_purple, interval=0.0001)
+            restartTry = True
+
+
+def SendView(item_id, proxy, timeout, proxytype):
+    global TotalSendedShare, TotalFailedReq, DebugMode
+    proxy         = {f'{proxytype}': f'{proxytype}://{proxy}'}
+    platform      = choice(Platforms)
+    osVersion     = randint(1, 12)
+    DeviceType    = choice(DeviceTypes)
+    headers       = {
+                        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+                        "user-agent": choice(UserAgent)
+                    }
+    appName       = choice(["tiktok_web", "musically_go"])
+    Device_ID     = randint(1000000000000000000, 9999999999999999999)
+    apiDomain     = choice(ApiDomain)
+    channelLol    = choice(Channel)
+    URI           = f"https://{apiDomain}/aweme/v1/aweme/stats/?channel={channelLol}&device_type={DeviceType}&device_id={Device_ID}&os_version={osVersion}&version_code=220400&app_name={appName}&device_platform={platform}&aid=1988"
+    Data          = f"item_id={item_id}&share_delta=1"
+
+    try:
+        req = r.post(URI, headers=headers, data=Data, proxies=proxy,timeout=timeout, stream=True, verify=False)
+        try:
+            if (req.json()["status_code"] == 0):
+                impr_id = req.json()["log_pb"]["impr_id"]
+                TotalSendedShare += 1
+                if DebugMode == True:
+                    print(Colorate.Horizontal(Colors.green_to_white, f"Sended Share: {TotalSendedShare} ({impr_id})"))
+                else:
+                    print(Colorate.Horizontal(Colors.green_to_white, f"Sended Share: {TotalSendedShare} ({impr_id})"))
+                    Title(f"Thread :{str(active_count()-1)} / Hit :{TotalSendedShare} / Fail :{TotalFailedReq}")
+            else:
+                pass
+        except:
+            TotalFailedReq += 1
+            Title(f"Thread :{str(active_count()-1)} / Hit :{TotalSendedShare} / Fail :{TotalFailedReq}")
+    except:
+        pass
+
+def ClearURI(link):
+    if ("vm.tiktok.com" in itemID or "vt.tiktok.com" in itemID):
+        return r.head(itemID, stream=True, verify=False, allow_redirects=True, timeout=5).url.split("/")[5].split("?", 1)[0]
+    else:
+        return itemID.split("/")[5].split("?", 1)[0]
+
+if (__name__ == "__main__"):
+    Clear()
+    itemID       = Write.Input("Video Link > ", Colors.red_to_purple, interval=0.0001)
+    amount       = Write.Input("Amount (0=inf) > ", Colors.red_to_purple, interval=0.0001)
+    ScrapProxie  = Write.Input("Scrap Proxies [y/n] > ", Colors.red_to_purple, interval=0.0001)
+    Proxytype    = Write.Input("Proxy Type > ", Colors.red_to_purple, interval=0.0001)
+    Timeout      = Write.Input("Proxy Timeout > ", Colors.red_to_purple, interval=0.0001)
+    NThread      = Write.Input("Thread Amount > ", Colors.red_to_purple, interval=0.0001)
+    
+    if Title("Proy Scrapper X-Proxy by NightFallGT") == True:
+        Debug = Write.Input("Debug Fails [y/n] ? > ", Colors.red_to_purple, interval=0.0001)
+        if Debug.lower().startswith("y"):
+            DebugMode = True
+        else:
+            DebugMode = False
+
+    if ScrapProxie.lower().startswith("y"):
+        from Data.ScrapProxie import Start; Start()
+
+    itemID = ClearURI(itemID)
+
+    ProxyChoose = True
+    while ProxyChoose:
+        if Proxytype.lower().startswith("h"):
+            Proxytype = "http"
+            ProxyChoose = False
+        elif Proxytype.lower().endswith("4"):
+            Proxytype = "socks4"
+            ProxyChoose = False
+        elif Proxytype.lower().endswith("5"):
+            Proxytype = "socks5"
+            ProxyChoose = False
+        else:
+            ProxyChoose = True
+            print("Invalid Proxy Type | Choose : Http, Socks4, Socks5")
+            Proxytype = Write.Input("Proxy Type > ", Colors.red_to_purple, interval=0.0001)
+
+    proxy = ReadProxiesFile()
+
+    if (int(amount) == 0):
+        while True:
+            Run = True
+            while Run:
+                if (active_count() <= int(NThread)):
+                    Thread(target=(SendView), args=(itemID,choice(proxy),int(Timeout),Proxytype,)).start()
+    else:
+       for _ in range(int(amount)):
+            Run = True
+            while Run:
+                if (active_count() <= int(NThread)):
+                    Thread(target=(SendView), args=(itemID,choice(proxy),Timeout,Proxytype,)).start()
